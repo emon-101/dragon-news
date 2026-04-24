69697286fd5a469f6a5ac4524ed354700cff3ec4
emon-101 <emon26.ugv@gmail.com>
2026-04-24 22:40:21 +0600

    added header and custom fonts

---
+++ src/app/(auth)/layout.jsx
@@ -0,0 +1,19 @@
+import Navbar from '@/components/shared/Navbar';
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
+
+const AuthLayout = ({children}) => {
+    return (
+        <div className={montserrat.className}>
+            <Navbar />
+            {children}
+        </div>
+    );
+};
+
+export default AuthLayout;
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import logo from "@/assets/logo.png"
+import { format } from "date-fns";
+import Image from "next/image";
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="text-center py-8 space-y-2">
+            <Image src={logo} width={300} height={200} alt="Logo" className="mx-auto h-auto" />
+            <p className="text-[#706F6F]">Journalism Without Fear or Favour</p>
+            <p>{format(new Date(), "EEEE, MMM dd, yyyy")}</p>
         </div>
     );
 };
