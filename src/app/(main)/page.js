d32013a9cd8cf8c10cc64c40608bc18a64bf9ccd
emon-101 <emon26.ugv@gmail.com>
2026-04-25 13:30:52 +0600

    fetch and display news categories on homepage

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,7 +1,25 @@
-import Image from "next/image";
 
-export default function Home() {
+const getCategories = async()=> {
+  const res = await fetch("https://openapi.programming-hero.com/api/news/categories");
+  const data = await res.json();
+  return data.data.news_category;
+}
+
+export default async function Home() {
+  const categories = await getCategories();
+  console.log(categories);
   return (
-    <h2 className="text-5xl bg-base-300 font-bold">Homepage</h2>
+    <div className="grid grid-cols-4 gap-3 lg:w-4/5 mx-auto">
+      <div>
+        <span  className="text-3xl font-bold">All Categories</span>
+        <ul className="flex flex-col gap-3 mt-6">
+          {
+            categories.map(category => <li key={category.category_id} className="bg-slate-100 font-semibold text-lg text-gray-700 p-2 text-center rounded-xl">{category.category_name}</li>)
+          }
+        </ul>
+      </div>
+      <div className="text-3xl font-bold bg-purple-100 col-span-2">News</div>
+      <div className="text-3xl font-bold bg-yellow-100">Social Icons</div>
+    </div>
   );
 }
